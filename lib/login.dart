f698ba240468762f13506b5da54bbbdaa4ecf1f2
MRSMHS <99160893+MRSMHS@users.noreply.github.com>
2022-03-09 09:09:01 -0800

    Update login.dart
    18-12 pc

--- lib/login.dart
+++ lib/login.dart
@@ -13,16 +13,28 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  static const titile_style=TextStyle(fontFamily: 'IRS',fontSize: 25,fontWeight:FontWeight.w800,color: Colors.blue,letterSpacing: 0.5 );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('ورود',style: TextStyle(color: Colors.black45,fontFamily: 'IRS',fontSize: 22),),),
+      appBar: AppBar(title: Text('ورود',style: TextStyle(color: Colors.black45,fontFamily: 'IRS',fontSize: 22),),
+      centerTitle: true,leading: Icon(Icons.arrow_back,color: Colors.black45,),),
       body: LoginUi(),),
       
     );
   }
 
   Widget LoginUi(){
-    return Card();
+    return Center(child: Column(
+      children: [
+        SizedBox(height: 60,),
+        Text('فروشگاه آنلاین من',style:titile_style ,),
+        SizedBox(height: 30,),
+        Container(height:320,width: 250, 
+        child: Card(shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10) ),elevation: 25,)),
+      ],
+    ));
   }
 }
