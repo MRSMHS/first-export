4e7af993d45d16b7654541655c67dc11aa4c8574
MRSMHS <99160893+MRSMHS@users.noreply.github.com>
2022-03-08 23:02:21 -0800

    Add files via upload
    section 8

--- lib/selector.dart
+++ lib/selector.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
-void main(){
+
+void maine(){
   return runApp(Selector());
 }
 
@@ -11,8 +13,29 @@ class Selector extends StatefulWidget {
 }
 
 class _SelectorState extends State<Selector> {
+static const titile_style=TextStyle(fontFamily: 'IRS',fontSize: 25,fontWeight:FontWeight.w800,color: Colors.blue,letterSpacing: 0.5 );
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('jkhjd'),),),);
+    return MaterialApp(debugShowCheckedModeBanner: false,
+    home: Scaffold(body: 
+    Center(child: Column(
+      children: [
+        SizedBox(height: 80,),
+        Text('خوش آمدید ',style: titile_style,),
+        SizedBox(height: 40,),
+        Image.asset('assets/welcome.gif'),
+        SizedBox(height: 20,),
+        TextButton(onPressed: (){}, child: Text('ورود',style: TextStyle(fontSize: 25),),style:TextButton.styleFrom(backgroundColor: Colors.white,primary: Colors.blueAccent,elevation: 10,
+        shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(15) ),fixedSize: Size(330,60)
+      
+    )),
+     TextButton(onPressed: (){}, child: Text('ثبت نام',style: TextStyle(fontSize: 25)),style:TextButton.styleFrom(backgroundColor: Colors.blueAccent,primary: Colors.white,elevation: 10,
+        shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(15) ),fixedSize: Size(330,60)
+      
+    ))],),),)
+      
+    );
   }
 }
